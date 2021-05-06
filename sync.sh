522a6a15ef1ac0da048a55f1966742f9c959b25e
hayao <hayao@fascode.net>
2021-05-06 23:13:59 +0900

    [add] : Added packages

--- sync.sh
+++ sync.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 json="$(curl -Ls "https://aur.archlinux.org/rpc/?v=5&type=search&by=maintainer&arg=hayao")"
+script_path="$( cd -P "$( dirname "$(readlink -f "${0}")" )" && pwd )"
 
 package_list=($(echo "${json}" | jq -r ".results[] | .Name"))
 
